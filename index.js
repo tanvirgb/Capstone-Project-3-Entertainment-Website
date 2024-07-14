6418d938479f5c972b4f692fcf0d0fdf1368fce8
tanvirgb <tspramanik123@gmail.com>
2024-07-15 00:37:52 +0600

    Make Homepage

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public")); // Serve static files on port 3000 (default  port 3000 if not specified)
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.render("index.ejs", { test: "Hello world!" });
 });
 
+// Start the server on port 3000 and log a success message.
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
