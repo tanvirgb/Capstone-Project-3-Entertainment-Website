e6c50b491ff723e5929db6e5d58c38a3b743469e
tanvirgb <tspramanik123@gmail.com>
2024-07-16 22:18:27 +0600

    Make Game Page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public")); // Serve static files on port 3000 (default  port 3000 if not specified)
-app.get("/", async (req, res) => {
+app.get("/home", async (req, res) => {
   res.render("index.ejs"); // Render the index.ejs file with the requested data.
 });
 
@@ -15,6 +15,10 @@ app.get("/movie", async (req, res) => {
 
 app.get("/anime", async (req, res) => {
   res.render("anime.ejs"); // Render the anime.ejs file with the requested data.
+});
+
+app.get("/game", async (req, res) => {
+  res.render("game.ejs"); // Render the game.ejs file with the requested data.
 });
 // Start the server on port 3000 and log a success message.
 app.listen(port, () => {
